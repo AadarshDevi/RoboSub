c8cff1161965aa32462b0d323ff66ae2557269a3
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-19 17:25:27 -0700

    added a Log4J logger

--- src/main/java/org/aadarshdevi/Launcher.java
+++ src/main/java/org/aadarshdevi/Launcher.java
@@ -1,13 +1,16 @@
 package org.aadarshdevi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class Launcher
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Launcher {
+    public static final Logger logger = LogManager.getLogger(Launcher.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
     }
 }
