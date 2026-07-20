630850882e3aa42284c4a00073d5fe2cc274c89d
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-19 20:02:50 -0700

    reformatted code

--- src/main/java/org/aadarshdevi/Launcher.java
+++ src/main/java/org/aadarshdevi/Launcher.java
@@ -1,16 +1,23 @@
 package org.aadarshdevi;
 
+import nu.pattern.OpenCV;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.opencv.core.Mat;
 
-/**
- * Hello world!
- *
- */
 public class Launcher {
     public static final Logger logger = LogManager.getLogger(Launcher.class);
 
-    public static void main(String[] args) {
-        logger.info("Hello World!");
+    static void main(String[] args) {
+        logger.info("Project Running");
+
+        OpenCV.loadLocally();
+        logger.info("OpenCV Initialized");
+
+        Mat img = OCVTest.ImageTest.loadImage("C:/Users/CryosArtic/Downloads/Photos-1-001/PXL_20260711_222452843.MP.jpg");
+        OCVTest.ImageTest.saveImage(img, "D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/output/image.jpg");
+        logger.info("Image Saved and Written to Disk");
     }
+
+
 }
