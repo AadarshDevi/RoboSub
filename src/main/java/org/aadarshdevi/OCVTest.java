e6ad29aab33b6da9089b87536d6416817727eb9c
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-21 16:12:02 -0700

    moved code for making a box around a face from an image to ocv test

--- src/main/java/org/aadarshdevi/OCVTest.java
+++ src/main/java/org/aadarshdevi/OCVTest.java
@@ -26,5 +26,52 @@ public class OCVTest {
         }
     }
 
+    static class ImageFaceDetection {
+        public static void detectFace() throws URISyntaxException {
+            // load image
+            Mat imgLoaded = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222338052.jpg");
+            logger.info("Image loaded");
+
+            // store faces in a MatOfRect obj
+            MatOfRect facesDetected = new MatOfRect();
+
+            // CascadeClassifier detects the faces
+            CascadeClassifier cc = new CascadeClassifier();
+            int minFaces = Math.round(imgLoaded.rows() * 0.1f);
+
+            // url to the face detection config file
+            URL url = Launcher.class.getResource("haarcascade_frontalface_default.xml");
+            logger.info(url);
+
+            // filepath of the image
+            String filename = Paths.get(url.toURI()).toString();
+
+            // load image to face detector
+            cc.load(filename);
+
+            // detect and draw rectangles on faces
+            cc.detectMultiScale(
+                    imgLoaded,
+                    facesDetected,
+                    1.1,
+                    3,
+                    Objdetect.CASCADE_SCALE_IMAGE,
+                    new Size(minFaces, minFaces),
+                    new Size()
+            );
+
+            // store the data of the faces
+            Rect[] facesArray = facesDetected.toArray();
+
+            // draw a red border around the faces
+            for (Rect face : facesArray) {
+                Imgproc.rectangle(imgLoaded, face.tl(), face.br(), new Scalar(0, 0, 255), 10);
+            }
+
+            // save the image
+            OCVTest.ImageTest.saveImage(imgLoaded, "D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/output/boxed.jpg");
+        }
+    }
+
 
 }
