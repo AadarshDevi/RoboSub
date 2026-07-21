866751aab01ca6801dad1a55fd8609260ef71a20
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-21 16:15:21 -0700

    moving code

--- src/main/java/org/aadarshdevi/Launcher.java
+++ src/main/java/org/aadarshdevi/Launcher.java
@@ -3,20 +3,30 @@ package org.aadarshdevi;
 import nu.pattern.OpenCV;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.opencv.core.Mat;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 public class Launcher {
     public static final Logger logger = LogManager.getLogger(Launcher.class);
 
-    static void main(String[] args) {
+    static void main(String[] args) throws MalformedURLException, URISyntaxException {
         logger.info("Project Running");
 
         OpenCV.loadLocally();
         logger.info("OpenCV Initialized");
 
-        Mat img = OCVTest.ImageTest.loadImage("C:/Users/CryosArtic/Downloads/Photos-1-001/PXL_20260711_222452843.MP.jpg");
+        // save and load image
+        /*
+
+        Mat img = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222452843.MP.jpg");
         OCVTest.ImageTest.saveImage(img, "D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/output/image.jpg");
         logger.info("Image Saved and Written to Disk");
+        */
+
+        // face detection
+        // OCVTest.ImageFaceDetection.detectFace();
+
     }
 
 
