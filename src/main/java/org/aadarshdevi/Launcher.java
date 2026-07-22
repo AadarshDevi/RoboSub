ae6357bf4d8c6cec5c1b20748af644497d26990e
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-21 18:02:55 -0700

    fixed video processing from OCVTest.<Internal Class> to CamerProcess and ImageProcess

--- src/main/java/org/aadarshdevi/Launcher.java
+++ src/main/java/org/aadarshdevi/Launcher.java
@@ -1,28 +1,38 @@
 package org.aadarshdevi;
 
+import javafx.application.Platform;
 import nu.pattern.OpenCV;
+import org.aadarshdevi.learn.CameraProcess;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 public class Launcher {
     public static final Logger logger = LogManager.getLogger(Launcher.class);
 
-    static void main(String[] args) throws MalformedURLException, URISyntaxException {
+    static void main(String[] args) {
         logger.info("Project Running");
 
         OpenCV.loadLocally();
         logger.info("OpenPNP Initialized");
 
-//         save and load image
+//        save and load image
 //        Mat img = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222452843.MP.jpg");
 //        OCVTest.ImageTest.saveImage(img, "D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/output/image.jpg");
 //        logger.info("Image Saved and Written to Disk");
 
 //         face detection
-//         OCVTest.ImageFaceDetection.detectFace();
+//         Mat imgLoaded = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222338052.jpg");
+//         OCVTest.ImageFaceDetection.detectFace(imgLoaded);
+
+        // real time face detection
+        Platform.startup(() -> {
+            // Put any code that creates JavaFX components (like Scene) in here
+            CameraProcess cameraProcess = new CameraProcess();
+            cameraProcess.displayImage(cameraProcess.captureImage());
+
+            logger.info("JavaFX Initialized");
+        });
+
 
     }
 
