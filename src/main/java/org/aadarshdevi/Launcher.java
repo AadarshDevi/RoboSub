5c64f4b4887913b70641dc28708e1e25f81523c3
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-21 18:32:29 -0700

    moved code from Launcher to ImageRunner for easy changing of testing classes

--- src/main/java/org/aadarshdevi/Launcher.java
+++ src/main/java/org/aadarshdevi/Launcher.java
@@ -1,8 +1,6 @@
 package org.aadarshdevi;
 
-import javafx.application.Platform;
-import nu.pattern.OpenCV;
-import org.aadarshdevi.opencv.CameraProcess;
+import org.aadarshdevi.opencv.ImageRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,30 +8,7 @@ public class Launcher {
     public static final Logger logger = LogManager.getLogger(Launcher.class);
 
     static void main(String[] args) {
-        logger.info("Project Running");
-
-        OpenCV.loadLocally();
-        logger.info("OpenPNP Initialized");
-
-//        save and load image
-//        Mat img = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222452843.MP.jpg");
-//        OCVTest.ImageTest.saveImage(img, "D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/output/image.jpg");
-//        logger.info("Image Saved and Written to Disk");
-
-//         face detection
-//         Mat imgLoaded = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222338052.jpg");
-//         OCVTest.ImageFaceDetection.detectFace(imgLoaded);
-
-        // real time face detection
-        Platform.startup(() -> {
-            // Put any code that creates JavaFX components (like Scene) in here
-            CameraProcess cameraProcess = new CameraProcess();
-            cameraProcess.displayImage(cameraProcess.captureImage());
-
-            logger.info("JavaFX Initialized");
-        });
-
-
+        ImageRunner.run();
     }
 
 
---
+++ src/main/java/org/aadarshdevi/opencv/ImageRunner.java
@@ -0,0 +1,37 @@
+package org.aadarshdevi.opencv;
+
+import javafx.application.Platform;
+import nu.pattern.OpenCV;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ImageRunner {
+
+    public static final Logger logger = LogManager.getLogger(ImageRunner.class);
+
+    public static void run() {
+        logger.info("Project Running");
+
+        OpenCV.loadLocally();
+        logger.info("OpenPNP Initialized");
+
+//        save and load image
+//        Mat img = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222452843.MP.jpg");
+//        OCVTest.ImageTest.saveImage(img, "D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/output/image.jpg");
+//        logger.info("Image Saved and Written to Disk");
+
+//         face detection
+//         Mat imgLoaded = OCVTest.ImageTest.loadImage("D:/College/Miramar Community College/Clubs/Miramar Engineering Club/2027_RoboSub/RoboSub/src/main/resources/org/aadarshdevi/test_img/PXL_20260711_222338052.jpg");
+//         OCVTest.ImageFaceDetection.detectFace(imgLoaded);
+
+        // real time face detection
+        Platform.startup(() -> {
+            // Put any code that creates JavaFX components (like Scene) in here
+            CameraProcess cameraProcess = new CameraProcess();
+            cameraProcess.displayImage(cameraProcess.captureImage());
+
+            logger.info("JavaFX Initialized");
+        });
+    }
+
+}
