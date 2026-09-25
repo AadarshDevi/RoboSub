35f9f69b7bf972876f0573ad2511f046d3fcb3b5
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-24 17:38:44 -0700

    removed jetbrains annotation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,7 @@ module robosub {
     requires javafx.graphics;
     requires opencv;
     requires org.apache.logging.log4j;
-    requires org.jetbrains.annotations;
     requires static lombok;
-
-    opens org.aadarshdevi.learn.opencv to org.jetbrains.annotations;
 
     exports org.aadarshdevi.learn.opencv;
     exports org.aadarshdevi;
